24a75dc743e812db001d0aff40386d63e0765228
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2023-12-31 21:55:35 +0600

    string methods

---
+++ Day_1 (31.12.23)/main.py
@@ -0,0 +1,36 @@
+# print('Hello world!')
+
+
+# string
+# firstName = "Tanvir"
+# lastName = "Hasan Emon"
+# fullName = firstName+" "+lastName
+# print("Hello "+fullName)
+
+
+# integer
+# age = 21
+# print("Your age is "+str(age))
+
+
+# float
+# height = 5.6
+# print(height)
+# print(type(height))
+
+
+# declare multiple variable
+# firstName, lastName = "Tanvir", "Hasan"
+# print(firstName, lastName)
+a = b = c = 5
+# print(a, b, c)
+
+
+# string methods
+# name = "Tanvir Hasan Emon"
+# print(len(name))
+# print(name.find('Emon'))
+# print(name.isdigit())
+# print(name.isalpha())
+# print(name.count('n'))
+# print(name.replace("a","z"))
