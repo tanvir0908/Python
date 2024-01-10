e3aa7c42fc4c363eb599af5bd89902e0ae805a78
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2024-01-11 01:51:49 +0600

    completed parity program in lecture 1

---
+++ Lecture 1/parity.py
@@ -0,0 +1,9 @@
+def isEven(number):
+    if number % 2 == 0:
+        print("This is an even number")
+    else:
+        print("This is an odd number")
+
+
+number = int(input("Enter a number: "))
+isEven(number)
